b9e84a73f6df2d2882cb1ab5826e94c0ce927bf0
jegyed50 <26919968+jegyed50@users.noreply.github.com>
2021-12-25 09:03:24 +0100

    A dallam B számolás 1 to n

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 input.onButtonPressed(Button.A, function () {
-    led.setBrightness(55)
     basic.showLeds(`
         # . . . .
         . . . . .
@@ -83,7 +82,32 @@ input.onButtonPressed(Button.A, function () {
     music.playTone(294, music.beat(BeatFraction.Quarter))
     music.playTone(294, music.beat(BeatFraction.Quarter))
     music.playTone(262, music.beat(BeatFraction.Half))
+    basic.pause(5000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
 })
+input.onButtonPressed(Button.B, function () {
+    if (Szam < LegnagyobbSzam) {
+        Szam += 1
+    } else {
+        Szam = 1
+    }
+    AkcioAdottSzamra(Szam)
+})
+function AkcioAdottSzamra (szám: number) {
+    basic.showNumber(Szam)
+    for (let index = 0; index <= Szam - 1; index++) {
+        music.playTone(262, music.beat(BeatFraction.Half))
+        basic.pause(750)
+    }
+}
+let Szam = 0
+let LegnagyobbSzam = 0
 basic.showLeds(`
     . . . . .
     # . . . #
@@ -91,6 +115,9 @@ basic.showLeds(`
     . . # . .
     . . . . .
     `)
+LegnagyobbSzam = 5
+Szam = 0
+led.setBrightness(50)
 basic.forever(function () {
 	
 })
